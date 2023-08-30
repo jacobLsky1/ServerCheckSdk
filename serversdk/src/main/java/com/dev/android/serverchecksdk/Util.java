61a4dfa911bd2a78a03f71eec559bc54a6421011
Jacob Lipsky <jacob.lipsky1@gmail.com>
2023-08-30 10:11:03 +0300

    update

--- serversdk/src/main/java/com/dev/android/serverchecksdk/Util.java
+++ serversdk/src/main/java/com/dev/android/serverchecksdk/Util.java
@@ -10,7 +10,6 @@ public class Util {
         public String sportBackup = "aHR0cHM6Ly93d3cuZXNwbi5jb20v";
         public String backUpSite = "";
         public String userAgentInfo = "";
-        public String SportsAPI = "";
     }
 
     public static Companion companion = new Companion();
